9e9a74fd724d2c3a5652472628cea8ecd2958956
Sandesh <136434467+sandeshdesai07@users.noreply.github.com>
2025-08-05 18:57:55 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -63,7 +63,7 @@ with st.container():
     st.write("\n")
     st.write("Upload your dataset and explore its structure and basic statistics.")
     st.markdown(
-    "<h4 style='text-align: center;'>Upload your dataset and explore its structure and basic statistics.</h4>",
+    "<h5 style='text-align: center;'>Upload your dataset and explore its structure and basic statistics.</h5>",
     unsafe_allow_html=True
 )
     uploaded_file = st.file_uploader("📂 Upload your CSV file", type=["csv"])
